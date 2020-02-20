11438f805d7b50416299b9ce9614429a8f174f97
Rohan Katakam <rohankatakam@yahoo.com>
2020-02-20 15:40:38 -0600

    UI Update
    -Updated form UI
    -Updated matrix item styling (Bootstrap badge)

--- backend.js
+++ backend.js
@@ -11,24 +11,22 @@ function addItem(){
     
     var custom_id = "matrix-item-" + (Math.random().toString(36).substr(2, 9)).toString();
     
+    var item_html = "<span class='badge badge-light matrix-item' draggable='true'  ondragstart='drag(event)'" + "id=" + "'" + custom_id + "' onclick='deleteItem(this.id)'>" + info + "</span>";
+    
     if(category == "Do Now"){
-        document.getElementById("topLeftContainer").innerHTML += 
-            "<div class='matrix-item' draggable='true'  ondragstart='drag(event)'" + "id=" + "'" + custom_id + "' onclick='deleteItem(this.id)'>" + info + "</div>";
+        document.getElementById("topLeftContainer").innerHTML += item_html;
     }
     
     if(category == "Schedule"){
-        document.getElementById("topRightContainer").innerHTML += 
-            "<div class='matrix-item' draggable='true'  ondragstart='drag(event)'" + "id=" + "'" + custom_id + "' onclick='deleteItem(this.id)'>" + info + "</div>";
+        document.getElementById("topRightContainer").innerHTML += item_html;
     }
     
     if(category == "Delegate"){
-        document.getElementById("bottomLeftContainer").innerHTML += 
-            "<div class='matrix-item' draggable='true'  ondragstart='drag(event)'" + "id=" + "'" + custom_id + "' onclick='deleteItem(this.id)'>" + info + "</div>";
+        document.getElementById("bottomLeftContainer").innerHTML += item_html;
     }
     
     if(category == "Eliminate"){
-        document.getElementById("bottomRightContainer").innerHTML += 
-            "<div class='matrix-item' draggable='true'  ondragstart='drag(event)'" + "id=" + "'" + custom_id + "' onclick='deleteItem(this.id)'>" + info + "</div>";
+        document.getElementById("bottomRightContainer").innerHTML += item_html;
     }
 }
 
@@ -75,6 +73,40 @@ function drag(ev) {
 function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+    if((ev.target.id == "topLeftContainer") || (ev.target.id == "topRightContainer") || (ev.target.id == "bottomLeftContainer") || (ev.target.id == "bottomRightContainer")){
+        ev.target.appendChild(document.getElementById(data));
+    }
     
+}
+
+function styleModal(quadrant){
+    var header = document.getElementsByClassName("modal-header")[0];
+    
+    var body = document.getElementsByClassName("modal-body")[0];
+    
+    var footer = document.getElementsByClassName("modal-footer")[0];
+    
+    if(quadrant == "Do Now"){
+        header.style.backgroundColor = "#94cf60";
+        body.style.backgroundColor = "#b0dc89";
+        footer.style.backgroundColor = "#94cf60";
+    }
+    
+    if(quadrant == "Schedule"){
+        header.style.backgroundColor = "#3199b8";
+        body.style.backgroundColor = "#4ab0cf";
+        footer.style.backgroundColor = "#3199b8";
+    }
+    
+    if(quadrant == "Delegate"){
+        header.style.backgroundColor = "#ebaa50";
+        body.style.backgroundColor = "#efbd76";
+        footer.style.backgroundColor = "#ebaa50";
+    }
+    
+    if(quadrant == "Eliminate"){
+        header.style.backgroundColor = "#b03146";
+        body.style.backgroundColor = "#cd4c61";
+        footer.style.backgroundColor = "#b03146";
+    }
 }
